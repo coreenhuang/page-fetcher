2704f83bcd81fe31ac3e62945965c805e1191b99
Coreen Huang <coreen.huang@gmail.com>
2022-09-04 16:44:18 -0400

    functioning fetcher for page downloader

--- fetcher.js
+++ fetcher.js
@@ -6,21 +6,18 @@ let url = args[0];
 let localFilePath = args[1];
 
 
-request(url, (error) => {
+request(url, (error, response, body) => {
+
   if (error) {
     console.log('error:', error); 
+  } else {
+
+    fs.writeFile(localFilePath, body, (error) => {
+      if (error) {
+        console.log('error:', error); 
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`); 
+    })
   }
- 
-  fs.writeFile(localFilePath, response, (error) => {
-
-    if (error) {
-      console.log('error:', error); 
-    }
-
-    let fileSize = 
-
-    console.log(`Downloaded and saved ${fileSize} bytes to ${localFilePath}`); 
-
-  })
 
 });
